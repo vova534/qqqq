abc560500419ce5eb66951c7bde42a6f7e7c538c
Vova <kagramanyan.007@mail.ru>
2021-06-10 19:20:16 +0300

    Initial Commit

---
+++ Shared/qqqqApp.swift
@@ -0,0 +1,17 @@
+//
+//  qqqqApp.swift
+//  Shared
+//
+//  Created by Владимир Каграманян on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct qqqqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
